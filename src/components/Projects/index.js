4390190e88af51d38fe83f5dd40bbfbcbd7c9a46
Emnetdegafe <emnetdegafe@gmail.com>
2021-01-09 01:02:50 +0100

    fix project page look

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,77 +1,76 @@
 import React, { useState } from "react";
-import { Carousel } from "react-bootstrap";
 import "./Projects.scss";
 import { useParams } from "react-router-dom";
 
+
+
 export default function Projects() {
   const param = useParams();
-  console.log("params", param);
 
+  const projects = [{
+    projectName: " AllesOverFilm",
+    gitLink: "https://github.com/Emnetdegafe/allesOverFilm-client",
+    discription: " A React Native app with bar code reader functionality used to compare prices of BluRays and DVDs with online stores.",
+    tech: "react-native, node.js, express, Expo, PostgreSQL, Sequalize, RESTful Apis",
+    imageUrl: "../../images/AllesOverFilm.jpg"
+  }, {
+    projectName: " FlashCards Learning Tool",
+    gitLink: "https://github.com/Emnetdegafe/allesOverFilm-client",
+    discription: "A learning tool to help students study using flash cards. It also has a Q and A game like function to help them remember content of their subjects.",
+    tech: "React, React-Redux, SASS, CSS",
+    imageUrl: "../../images/FlashCard.jpg"
+  },
+  {
+    projectName: " The Idealists",
+    gitLink: "https://github.com/dashakrolik/Idealists-client",
+    discription: "I worked with a team of 5 to add a co-founder section of their existing plateform that turns selected ideas into future companies.",
+    tech: "React, TypeORMs, StyledComponents, Material UI",
+    imageUrl: "../../images/Idealists.png"
+  },
+    // {
+    //     projectName: "Konjo Bet Home Styling",
+    //     gitLink: "",
+    //     discription: "text 3", 
+    //     tech: "HTML, CSS, JavaScript",
+    //     imageUrl: ""
+
+    //   }
+  ]
   return (
-    <div className="project-container">
+
+    <div className="projects-container">
       <h4 className="About-intro">projects</h4>
-      <Carousel>
-        <Carousel.Item>
-          <a
-            href="https://github.com/Emnetdegafe/allesOverFilm-client"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              className="project1"
-              src="images/AllesOverFilm.jpg"
-              alt="AllesOverFilm"
-            />
-          </a>
-          <Carousel.Caption>
-            <h4 className="caption-title">AllesOverFilm Mobile App</h4>
-            <p>
-              Mobile app with bar code functionality used to compare prices of
-              items with online stores.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <a
-            href="https://fervent-hugle-a9038a.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              className="project2"
-              src={"/images/FlashCard.jpg"}
-              alt="Flash Card learning tool"
-            />
-          </a>
+      <div className="projects-wrapper">
+        {projects.map((project, index) => {
+          console.log('projects', project.projectName)
 
-          <Carousel.Caption>
-            <h4 className="caption-title">Flash Card App</h4>
-            <p>
-              Learning tool to help students study using flash cards. It also
-              have a Q and A game like function to help them remember content of
-              their subjects.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <a
-            herf="https://mystifying-newton-a39828.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              className="project3"
-              src="images/Own_site.jpg"
-              alt="Personal Site"
-            />
-          </a>
-
-          <Carousel.Caption>
-            <h4 className="caption-title">Personal Site</h4>
-            <p>personal portfolio site made using React, SCSS and CSS.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+          return (
+            <div className="flip-card" key={index}>
+              <div className="flip-card-inner">
+                <a
+                  href={project.gitLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="card-front">
+                    <h4>{project.projectName}</h4>
+                    <img
+                      className="project-img"
+                      src={project.imageUrl}
+                      alt="AllesOverFilm"
+                    />
+                  </div>
+                  <div className="card-back">
+                    <h4 className="caption-title">Discription</h4>
+                    <p>{project.discription}</p>
+                    <span>Tech: {project.tech}</span>
+                  </div>
+                </a>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
